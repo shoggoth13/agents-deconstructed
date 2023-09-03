7e2ae7a6bd5ddde7cb184407576fdfd636bf26f8
shoggoth13 <139695466+shoggoth13@users.noreply.github.com>
2023-09-02 19:44:45 -0700

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="agents_deconstructed",
+    version="0.0.1",
+    description="Utils for running 'agents' where the core logic is not obfuscated.",
+    packages=find_packages(),
+    install_requires=[
+        "langchain",
+    ],
+)
